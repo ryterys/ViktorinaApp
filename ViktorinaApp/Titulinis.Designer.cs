6db7ed3c65b998ca632883dc1b4b6e5e57abde6e
Justas Garbačius <justas.garbacius@gmail.com>
2015-01-27 17:20:00 +0200

    Created quiz title screen for user input

--- ViktorinaApp/Titulinis.Designer.cs
+++ ViktorinaApp/Titulinis.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtVardas = new System.Windows.Forms.TextBox();
+            this.lblIvesti = new System.Windows.Forms.Label();
+            this.btnZaisti = new System.Windows.Forms.Button();
+            this.btnLenta = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // txtVardas
+            // 
+            this.txtVardas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtVardas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txtVardas.Location = new System.Drawing.Point(149, 73);
+            this.txtVardas.Name = "txtVardas";
+            this.txtVardas.Size = new System.Drawing.Size(271, 26);
+            this.txtVardas.TabIndex = 0;
+            this.txtVardas.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblIvesti
+            // 
+            this.lblIvesti.AutoSize = true;
+            this.lblIvesti.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.lblIvesti.Location = new System.Drawing.Point(123, 45);
+            this.lblIvesti.Name = "lblIvesti";
+            this.lblIvesti.Size = new System.Drawing.Size(337, 25);
+            this.lblIvesti.TabIndex = 1;
+            this.lblIvesti.Text = "PRAŠOME ĮVESTI SAVO VARDĄ:";
+            // 
+            // btnZaisti
+            // 
+            this.btnZaisti.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btnZaisti.Location = new System.Drawing.Point(233, 105);
+            this.btnZaisti.Name = "btnZaisti";
+            this.btnZaisti.Size = new System.Drawing.Size(104, 47);
+            this.btnZaisti.TabIndex = 2;
+            this.btnZaisti.Text = "ŽAISTI";
+            this.btnZaisti.UseVisualStyleBackColor = true;
+            // 
+            // btnLenta
+            // 
+            this.btnLenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btnLenta.Location = new System.Drawing.Point(449, 203);
+            this.btnLenta.Name = "btnLenta";
+            this.btnLenta.Size = new System.Drawing.Size(104, 47);
+            this.btnLenta.TabIndex = 3;
+            this.btnLenta.Text = "LENTA";
+            this.btnLenta.UseVisualStyleBackColor = true;
+            // 
+            // Titulinis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(621, 262);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(565, 262);
+            this.Controls.Add(this.btnLenta);
+            this.Controls.Add(this.btnZaisti);
+            this.Controls.Add(this.lblIvesti);
+            this.Controls.Add(this.txtVardas);
+            this.Name = "Titulinis";
+            this.Text = "TITULINIS";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtVardas;
+        private System.Windows.Forms.Label lblIvesti;
+        private System.Windows.Forms.Button btnZaisti;
+        private System.Windows.Forms.Button btnLenta;
     }
 }
 
